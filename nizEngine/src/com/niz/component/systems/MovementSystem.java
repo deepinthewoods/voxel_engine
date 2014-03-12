7e00cc14e9adfb6a77c86fa27ef12f364316c384
niz <niz@niz.niz>
2014-03-12 20:21:06 +0000

    speed limit change

--- nizEngine/src/com/niz/component/systems/MovementSystem.java
+++ nizEngine/src/com/niz/component/systems/MovementSystem.java
@@ -40,14 +40,14 @@ public class MovementSystem extends EntityProcessingSystem {
 	static Vector3 tmp = new Vector3();
 	
 	
-	private static void move(float rotation, float speed, float speedLimit, float speedLimit2, Vector3 position, Vector3 oldPosition, Move move) {
+	private static void move(float rotation, float speed, float speedLimit, float speedLimit2, float jumpSpeed, Vector3 position, Vector3 oldPosition, Move move) {
 		//move.moving = true;
 		tmp.set(speed/20f, 0, 0);
 		tmp.rotate(-rotation, 0,1,0);
 		position.x += tmp.x;
 		position.z += tmp.z;
 		
-		tmp.add(oldPosition);
+		/*tmp.add(oldPosition);
 		//Gdx.app.log(TAG, "move"+tmp.dst2(position));
 		if (tmp.dst2(position) > speedLimit2){
 			tmp.sub(position);
@@ -57,9 +57,24 @@ public class MovementSystem extends EntityProcessingSystem {
 			tmp.rotate(-rotation, 0,1,0);
 			position.add(tmp);
 			//Gdx.app.log(TAG, "limit");
-		}
+		}*/
+		tmp.set(oldPosition).sub(position);
+		float 
+		vx = Math.abs(tmp.x)
+		, vy = Math.abs(tmp.y)
+		, vz = Math.abs(tmp.z);
 		
-		//Gdx.app.log(TAG, "move"+move.speed);
+		float dx = speedLimit/vx
+				, dy = (speedLimit*jumpSpeed)/vy
+				, dz = speedLimit / vz;
+
+		float alpha = Math.min(Math.min(dx, dy), dz);
+		if (alpha < 1f){
+			tmp.scl(-alpha);
+			position.set(oldPosition).add(tmp.x, tmp.y, 0);
+			Gdx.app.log(TAG, "move"+alpha);
+		}
+	
 	}
 	
 	
@@ -74,13 +89,13 @@ public class MovementSystem extends EntityProcessingSystem {
 			//Vector3 position = entity.get(Position.class).pos;
 			//if (body.onGround)
 			boolean left = c.rotation > 90 && c.rotation < 270;
-			position.set((left?-c.speedLimit:c.speedLimit), c.speedLimit*2.5f, 0).add(oldPosition);
+			position.set((left?-c.speedLimit:c.speedLimit), c.speedLimit*c.jumpStrength, 0).add(oldPosition);
 			c.jumpQueued = false;
 			
 		} else 
 		
 		if (c.moving){
-			move(c.rotation, c.speed, c.speedLimit, c.speedLimit * c.speedLimit, position, oldPosition, c);
+			move(c.rotation, c.speed, c.speedLimit, c.speedLimit * c.speedLimit, c.jumpStrength, position, oldPosition, c);
 		} else if (!c.moving && body.onGround){//apply friction
 			//Gdx.app.log(TAG, "friction");
 			tmp.set(position).sub(oldPosition);
